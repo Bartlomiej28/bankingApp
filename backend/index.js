d535f030eab249538101677069867ee00b4e5e22
Bartek <120282905+Bartlomiej28@users.noreply.github.com>
2024-09-03 22:25:28 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,22 @@ const app = express();
 
 app.use(express.json());
 
+app.options('*', cors({
+    origin: 'https://banking-app-xv2x-frontend.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin']
+}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://banking-app-xv2x-frontend.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Origin');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
+/*
 app.use(
     cors({
         origin: 'https://banking-app-xv2x-frontend.vercel.app',
@@ -16,7 +32,7 @@ app.use(
         allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin']
     })
 );
-
+*/
 app.listen(3000, ()=>{
     console.log("App is listening to port: 5000")
 })
