3946392b5533ec2d05ff9031d8341558a551e96e
Bartek <120282905+Bartlomiej28@users.noreply.github.com>
2024-09-03 22:02:02 +0200

    Update BuyDepositComponent.tsx

--- frontend/src/Components/BuyDepositComponent.tsx
+++ frontend/src/Components/BuyDepositComponent.tsx
@@ -41,7 +41,7 @@ function BuyDepositComponent() {
 
     try {
       setIsLoading(true)
-      const response = await axios.post('http://localhost:3000/deposit/new-deposit', {
+      const response = await axios.post('https://banking-app-beige.vercel.app/deposit/new-deposit', {
         ownerID: userID,
         amount,
         endData: formattedFutureDate,
