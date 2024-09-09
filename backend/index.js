5618c1b00c989b72b9e5e1630c158fbdcef2ae66
Bartłomiej Boczyło <bartlomiejboczylo@MacBook-Air-Bartomiej.local>
2024-09-09 09:34:28 +0200

    add cors

--- backend/index.js
+++ backend/index.js
@@ -7,66 +7,18 @@ import cardRoute from './routes/cardRoute.js'
 
 const app = express();
 
+app.use(cors())
+
 app.use(express.json());
 
-app.options('*', cors({
-    origin: 'https://banking-app-xv2x-frontend.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin', 'Authorization']
-}));
+// app.options('*', cors({
+//     origin: 'https://banking-app-xv2x-frontend.vercel.app',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type',]
+// }));
 
-app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://banking-app-xv2x-frontend.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Origin');
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-    next();
-});
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
-/*
-app.use(
-    cors({
-        origin: 'https://banking-app-xv2x-frontend.vercel.app',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin']
-    })
-);
-*/
-app.listen(3000, ()=>{
-    console.log("App is listening to port: 5000")
+app.listen(3000, () => {
+    console.log("App is listening to port: 3000")
 })
 
 
